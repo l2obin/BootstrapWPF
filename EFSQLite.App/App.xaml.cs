859c8c373811b3b3aae1dd9f6b5d90aeafbcc4c1
Robin Ang <robinang87@gmail.com>
2014-09-27 18:41:13 +0800

    Added Bootstrapper, responsibile for the initialization of an application built using Prism Library

--- EFSQLite.App/App.xaml.cs
+++ EFSQLite.App/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Practices.Prism;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -12,5 +13,11 @@ namespace EFSQLite.App
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            Bootstrapper bootstrapper = new Bootstrapper();
+            bootstrapper.Run();
+        }
     }
 }
